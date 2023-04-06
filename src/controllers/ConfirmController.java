2a8d05cc9d63ff5070ab6460c42c7dcb2796760c
Elaiah Asperin <esasperin@gmail.com>
2023-04-06 17:29:32 +0800

    Re-style home and confirmation pages to sass

--- src/controllers/ConfirmController.java
+++ src/controllers/ConfirmController.java
@@ -1,18 +1,37 @@
 package controllers;
+
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+
+import javafx.stage.Stage;
+
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.stage.Stage;
-import javafx.scene.input.MouseEvent;
 import javafx.scene.Node;
+import javafx.scene.text.Text;
+import javafx.scene.text.Font;
+import javafx.scene.input.MouseEvent;
 
-public class ConfirmController {
+import models.processors.StyleProcessor;
+
+public class ConfirmController implements Initializable {
     private Stage stage;
     private Scene scene;
     private Parent root;
+
+    @FXML private Text title;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        int font_size = StyleProcessor.getFontSize();
+        Font font = Font.loadFont(getClass().getResourceAsStream("../static/fonts/Heavyweight.ttf"), font_size * 7);
+        title.setFont(font);
+    }
     
     @FXML 
     protected void startTurn(MouseEvent e) throws IOException {
@@ -20,6 +39,7 @@ public class ConfirmController {
         stage = (Stage)((Node)e.getSource()).getScene().getWindow();
         scene = new Scene(root);
         stage.setScene(scene);
+        stage.setMaximized(true);
         stage.show();
     }
 }
--- src/controllers/HomeController.java
+++ src/controllers/HomeController.java
@@ -1,35 +1,38 @@
 package controllers;
 
-import models.processors.StyleProcessor;
-
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+
+import javafx.application.Platform;
+
+import javafx.stage.Stage;
+
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.stage.Stage;
-import javafx.scene.input.MouseEvent;
-import javafx.scene.text.Font;
-import javafx.scene.text.Text;
 import javafx.scene.Node;
+import javafx.scene.text.Text;
+import javafx.scene.text.Font;
+import javafx.scene.input.MouseEvent;
+
+import models.processors.StyleProcessor;
 
 public class HomeController implements Initializable {
     private Stage stage;
     private Scene scene;
     private Parent root;
 
-    @FXML private Text test;
+    @FXML private Text title;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         int font_size = StyleProcessor.getFontSize();
-        Font font = Font.loadFont(getClass().getResourceAsStream("../static/fonts/Heavyweight.ttf"), font_size * 4);
-        test.setFont(font);
+        Font font = Font.loadFont(getClass().getResourceAsStream("../static/fonts/Heavyweight.ttf"), font_size * 10);
+        title.setFont(font);
     }
     
     @FXML 
@@ -38,6 +41,7 @@ public class HomeController implements Initializable {
         stage = (Stage)((Node)e.getSource()).getScene().getWindow();
         scene = new Scene(root);
         stage.setScene(scene);
+        stage.setMaximized(true);
         stage.show();
     }
 
