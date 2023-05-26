c2232566b65ea96487566deb5a98ad30b066b47a
galacticus-underscore <93103804+galacticus-underscore@users.noreply.github.com>
2023-05-26 22:11:25 +0800

    Update AvatarDeathEvent.java
    Added Javadoc comments

--- src/models/events/AvatarDeathEvent.java
+++ src/models/events/AvatarDeathEvent.java
@@ -1,3 +1,10 @@
+/**
+* AvatarDeathEvent.java
+*
+* this event handles the occasion that a player's health reacher 0. The player who remains alive is acquired using the pointer Killer, 
+* and the player that died is acquired using the pointer Avatar. The killer is the winner of the session.
+**/
+
 package models.events;
 
 import models.enums.Pointers;
