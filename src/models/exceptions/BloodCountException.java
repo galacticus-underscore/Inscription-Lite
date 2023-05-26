4a87ffb2aec094405232b7f1d3ed02a5fb6af82f
galacticus-underscore <93103804+galacticus-underscore@users.noreply.github.com>
2023-05-26 10:19:45 +0800

    Update BloodCountException.java
    Added Javadoc comments

--- src/models/exceptions/BloodCountException.java
+++ src/models/exceptions/BloodCountException.java
@@ -1,3 +1,9 @@
+/**
+*
+* @throws BloodCountException < is thrown when a player's blood count is lower than the summoning cost of the card being played.> 
+*
+**/ 
+
 package models.exceptions;
 
 public class BloodCountException extends Exception {
