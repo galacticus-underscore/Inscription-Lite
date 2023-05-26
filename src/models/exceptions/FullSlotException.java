6f50e7a1a9d8529e38743ad1ad1467fe75fa62ec
galacticus-underscore <93103804+galacticus-underscore@users.noreply.github.com>
2023-05-26 21:09:23 +0800

    Update FullSlotException.java
    Added Javadoc comments

--- src/models/exceptions/FullSlotException.java
+++ src/models/exceptions/FullSlotException.java
@@ -1,3 +1,9 @@
+/**
+*
+* @throws FullSlotException < when a card is placed on a board slot with a card already occupying it >
+*
+**/
+
 package models.exceptions;
 
 public class FullSlotException extends Exception {
