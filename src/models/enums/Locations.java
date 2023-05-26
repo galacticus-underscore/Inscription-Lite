6479ff057b63e90d3d4a090e7a14c24329ff13a6
galacticus-underscore <93103804+galacticus-underscore@users.noreply.github.com>
2023-05-26 21:43:59 +0800

    Update Locations.java
    Added Javadoc comments

--- src/models/enums/Locations.java
+++ src/models/enums/Locations.java
@@ -1,6 +1,13 @@
+/**
+* Locations.java
+* 
+* declares the different cells game screen where images are to be incorporated
+* such as the grid where cards are placed, the deck and the graveyard.
+**/
+
 package models.enums;
 
 public enum Locations {
-    HA, HD, HH, H1, H2, H3, H4, HG,
+    HA HD, HH, H1, H2, H3, H4, HG,
     AA, AD, AH, A1, A2, A3, A4, AG;
 }
