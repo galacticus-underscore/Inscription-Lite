a59aceb5e2a88a37e79e9b230e6a5794cb12a9e0
galacticus-underscore <93103804+galacticus-underscore@users.noreply.github.com>
2023-05-26 22:20:29 +0800

    Update DrawEvent.java
    Added Javadoc comments

--- src/models/events/DrawEvent.java
+++ src/models/events/DrawEvent.java
@@ -1,3 +1,11 @@
+/**
+* DrawEvent.java
+*
+* this event occurs when the active player draws a card, and obtains a card from the deck-- acquiring its value
+* using the pointer PD. (active player's deck) That card is then placed into the active player's hand, by adding 
+* the card into the pointer PH. (active player's hand)
+**/
+
 package models.events;
 
 import models.enums.Pointers;
